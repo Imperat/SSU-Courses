d2106995858b4e4fec6ae336cb052e3012317897
Mikhail <mlelyakin@mirantis.com>
2016-09-17 22:23:45 +0300

    Fix #3 for CRC8 Algo

--- ssu-information-security/crc8.go
+++ ssu-information-security/crc8.go
@@ -11,6 +11,7 @@ func main() {
 	numbers := [8]byte{ 128, 64, 32, 16, 8, 4, 2, 1 }
 	// Polynom
 	var poly byte = 0xD5
+	var r byte = 0
     // Open file for reading
     file, err := os.Open("input.txt")
     if err != nil {
@@ -19,29 +20,32 @@ func main() {
 
     b_init := make([]byte, 1)
     file.Read(b_init)
-	log.Printf("%n", b_init[0])
     b_init[0] = b_init[0] ^ poly
-	log.Printf("Before sdvig: %n \n", b_init[0])
+	fmt.Printf("%n \n", b_init[0])
     b_init[0] = b_init[0] << 1
     for true {
         b := make([]byte, 1)
         _, err := file.Read(b)
         if err != nil {
 			for i := 0; i < 8; i++ {
-			    b_init[0] = b_init[0] ^ poly
-				b_init[0] = b_init[0] << 1
+			   b_init[0] = b_init[0] << 1
+			   b_init[0] = b_init[0] ^ poly
 			}
-            fmt.Printf("CRC8 code is %n \n", b_init[0])
             break
         }
         for i := 0; i < 8; i++ {
+			//b_init[0] = b_init[0] << 1
             is_one := b[0] & numbers[i]
             if is_one > 0 {
             	b_init[0] = b_init[0] + 1
 			}
-            b_init[0] = b_init[0] ^ poly
-			fmt.Printf("Before sdvig: %n \n", b_init[0])
-            b_init[0] = b_init[0] << 1
+			// Add if statement
+			r = (b_init[0] & 128) >> 7
+			if r == 1 {
+                b_init[0] = b_init[0] ^ poly
+			}
+			fmt.Printf("%n \n", b_init[0])
+			b_init[0] = b_init[0] << 1
         }
     }
 }
