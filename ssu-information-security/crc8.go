e5eca8b1616577b8a1545734636f50f7c176dad8
Mikhail <mlelyakin@mirantis.com>
2016-09-17 19:16:28 +0300

    Fix some bugs in CRC8

--- ssu-information-security/crc8.go
+++ ssu-information-security/crc8.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// Array helper
-	numbers := [8]byte{ 1, 2, 4, 8, 16, 32, 64, 128 }
+	numbers := [8]byte{ 128, 64, 32, 16, 8, 4, 2, 1 }
 	// Polynom
 	var poly byte = 0xD5
     // Open file for reading
@@ -19,24 +19,27 @@ func main() {
 
     b_init := make([]byte, 1)
     file.Read(b_init)
-    
+	log.Printf("%n", b_init[0])
     b_init[0] = b_init[0] ^ poly
     b_init[0] = b_init[0] << 1
-
     for true {
         b := make([]byte, 1)
         _, err := file.Read(b)
         if err != nil {
-            fmt.Printf("CRC8 code is %n \n", b_init)
+			for i := 0; i < 8; i++ {
+			    b_init[0] = b_init[0] ^ poly
+				b_init[0] = b_init[0] << 1
+			}
+            fmt.Printf("CRC8 code is %n \n", b_init[0])
             break
         }
         for i := 0; i < 8; i++ {
-            is_one := b[0] ^ numbers[i]
+            is_one := b[0] & numbers[i]
             if is_one == 1 {
             	b_init[0] = b_init[0] + 1
-            	b_init[0] = b_init[0] ^ poly
-            	b_init[0] = b_init[0] << 1
-            }
+			}
+            b_init[0] = b_init[0] ^ poly
+            b_init[0] = b_init[0] << 1
         }
     }
 }
